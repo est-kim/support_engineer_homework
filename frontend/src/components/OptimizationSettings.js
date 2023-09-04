1e950ce4041c8ced269e184a0aa5ab529dfb85c4
Esther Kim <kim.esther117@gmail.com>
2023-09-04 16:58:59 -0700

    OptimizationSettings component built

--- frontend/src/components/OptimizationSettings.js
+++ frontend/src/components/OptimizationSettings.js
@@ -1,9 +1,58 @@
+import { useState } from "react";
+
 function OptimizationSettings({ organizationData }) {
-    return (
-        <div>
-            <h1>Optimization Settings</h1>
-        </div>
+  const [domainInput, setDomainInput] = useState("");
+  const [optimizationSettings, setOptimizationSettings] = useState(null);
+  const [searchResult, setSearchResult] = useState(false);
+
+  const findOptimizationSettings = () => {
+    setSearchResult(true);
+    const org = organizationData.find(
+      (org) => org.myShopifyDomain.trim() === domainInput.trim()
     );
+
+    if (org) {
+      if (org.setup && org.setup.optimization) {
+        setOptimizationSettings(org.setup.optimization);
+      }
+    } else {
+      setOptimizationSettings(null);
+      console.warn("Organization not found for domain: ", domainInput);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      findOptimizationSettings();
+    }
+  }
+
+  return (
+    <div>
+      <h1>Optimization Settings</h1>
+      <input
+        type="text"
+        placeholder="Enter myShopifyDomain"
+        value={domainInput}
+        onChange={(e) => setDomainInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={findOptimizationSettings}>Search</button>
+
+      {optimizationSettings ? (
+        <div>
+          <pre>{JSON.stringify(optimizationSettings, null, 2)}</pre>
+        </div>
+      ) : (
+        <>
+            {searchResult && (
+                <p>No optimization settings found for the entered domain.</p>
+            )}
+            <p>Please enter a valid and/or existing domain. Example: test-account-1.myshopify.com</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default OptimizationSettings;
